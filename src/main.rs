de978c90e5b48a28ee0ac5c0dff78c34b04bc136
Krzysztof Goj <krzysztof.goj@gmail.com>
2021-12-21 12:55:24 +0100

    Implement get_focused_pid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use swayipc::{Connection, Fallible, Node};
+
+fn get_focused_pid(node: Node) -> Option<i32> {
+    if node.focused {
+        return node.pid;
+    }
+    let children = node.nodes.into_iter()
+        .chain(node.floating_nodes.into_iter());
+    children.flat_map(get_focused_pid).next()
+}
+
+fn main() -> Fallible<()> {
+    let mut connection = Connection::new()?;
+    let tree = connection.get_tree()?;
+    let focused_pid = get_focused_pid(tree);
+    println!("focused_pid: {:?}", focused_pid);
+    Ok(())
 }
